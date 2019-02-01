3392a8ddace81b5afc4ff24ba2e7e796fbc0e166
egoing <egoing@gmail.com>
2019-02-02 01:54:47 +0900

    전역 변수로 사용한 camtasiaJson이 다른 이벤트와 간섭을 일으키는 현상 해결

--- main.js
+++ main.js
@@ -17,12 +17,12 @@ updateProjectFileQueue.drain = function(){
 }
 // 변경 된 파일을 수정하면 다시 watch가 호출되기 때문에 작업을 해도 되는지 파악하기 위한 flag
 var isProcess = false;
-var camtasiaJson;
+
 wacher.on('all', (event, fpath) => {
     if(isProcess)
         return;
     console.info('>>> scan start', event, fpath);
-    camtasiaJson = JSON.parse(fs.readFileSync(fpath, 'utf8'));
+    var camtasiaJson = JSON.parse(fs.readFileSync(fpath, 'utf8'));
     if(camtasiaJson['sourceBin'] === undefined)
         return;
     camtasiaJson['sourceBin'].forEach((element, index) => {
